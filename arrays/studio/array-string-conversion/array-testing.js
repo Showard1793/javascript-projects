dd274e9db3bfce81d70739cf2925216952c56667
Stephen Howard <Showard1793@gmail.com>
2024-06-20 20:28:26 -0500

    Updated Array Testing Studio

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -5,17 +5,27 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
+//1)
+console.log(protoArray1.includes(","));
+console.log(protoArray2.includes(";"));
+console.log(protoArray3.includes(" "));
+console.log(protoArray4.includes(", "));
+
 //2) 
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
 	let check;
 	let output;
 	//TODO: 2. write the code required for this step
+let arr1 = protoArray1.split(",");
+console.log(arr1);
+output = arr1.reverse();
+console.log(arr1);
 
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
-
+reverseCommas();
 //3)
 function semiDash() {
 	let check;
