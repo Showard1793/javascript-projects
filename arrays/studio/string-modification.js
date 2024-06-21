53435a4f2cab38bf18fffb67a29098aafe7825a7
Stephen Howard <Showard1793@gmail.com>
2024-06-20 20:33:25 -0500

    Updated String Modification

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,43 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
 
-//1) Use string methods to remove the first three characters from the string and add them to the end.
-//Hint - define another variable to hold the new string or reassign the new string to str.
+//1) Use string methods to remove the first three characters from the string and add them 
+//to the end.
+//Hint - define another variable to hold the new string or reassign the new string to str. 
+let sliceOfNumbers = (str.slice(0,3));
+let slicedStr = str.slice(3,str.length);
+
+let combinedStr = slicedStr + sliceOfNumbers;
+console.log(combinedStr);
+
 
 //Use a template literal to print the original and modified string in a descriptive phrase.
+console.log(`${str} is the original and ${combinedStr} is the modified`);
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+numberOfCharacters = input.question("How many letters should I slice and paste? ");
+numberOfLetters = Number(numberOfCharacters);
+sliceOfNumbers = (str.slice(0,numberOfLetters));
+slicedStr = str.slice(numberOfLetters,str.length);
 
-//3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+combinedStr = slicedStr + sliceOfNumbers;
+console.log(combinedStr);
+
+//3) Add validation to your code to deal with user inputs that are longer than the word.
+// In such cases, default to moving 3 characters. Also, the template literal should note the
+// error.
+
+//If user enters too many letters
+if (numberOfLetters > str.length) {
+    sliceOfNumbers = (str.slice(0,3));
+    slicedStr = str.slice(3,str.length)
+    combinedStr = slicedStr + sliceOfNumbers;
+    console.log(`You have entered ${numberOfLetters}. That is longer than the word. Defaulting to slice of 3 
+    ${combinedStr}`)
+//User enters valid number
+} else { 
+    sliceOfNumbers = (str.slice(0,numberOfLetters));
+    slicedStr = str.slice(numberOfLetters,str.length);
+    combinedStr = slicedStr + sliceOfNumbers;
+    console.log(`${str} is the original and ${combinedStr} is the modified`);
+}
