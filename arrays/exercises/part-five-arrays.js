b21ee0606951be6d375789b44c4f4c987b362f5b
Stephen Howard <Showard1793@gmail.com>
2024-08-05 16:04:13 -0500

    1.0

--- arrays/exercises/part-five-arrays.js
+++ arrays/exercises/part-five-arrays.js
@@ -1,6 +1,7 @@
 let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
+//test
 //1) Use the split method on the string to identify the purpose of the parameter inside the ().
 str.split(" ");
 
